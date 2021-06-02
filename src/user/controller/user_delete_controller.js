da59ce421c029c11ee6bf7a0ac6b81681d6e998f
Vincent <bongrand.v91@gmail.com>
2021-06-02 18:15:31 +0200

    update user delete

--- src/user/controller/user_delete_controller.js
+++ src/user/controller/user_delete_controller.js
@@ -28,54 +28,25 @@ exports.deleteUser = async (req, res, next) => {
         return res.status(400).json({status : false, message : 'Error : User not deleted'})
     }
 
-    let postDelete
-    try {
-        postDelete = await Post.deleteMany({authorId : req._id});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Post not deleted'})
+    try {
+
+        await deletePost(req._id)
+    } catch (err){
+
+        console.log(err)
     }
 
-    let postComDelete
-    try {
-        postComDelete = await PostCom.deleteMany({authorId : req._id});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : UserCom not deleted'})
-    }
+    try {
 
-    let likeDelete
-    try {
-        likeDelete = await Like.deleteMany({userId : req._id});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Like not delete'})
-    }
+        await deleteTopic(req._id)
+    } catch (err){
 
-    let topicDelete
-    try {
-        topicDelete = await Topic.deleteMany({ownerId : req._id});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Topic not delete'})
-    }
-
-    let topicMessageDelete
-    try {
-        topicMessageDelete = await TopicMessage.deleteMany({authorId : req._id});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : TopicMessage not deletes'})
+        console.log(err)
     }
 
     let friendDelete
     try {
-        friendDelete = await Friend.deleteMany({receiverId : req._id});
+        friendDelete = await Friend.deleteMany({$or: [{ receiverId: req._id }, { senderId: req_id }]});
     } catch (err) {
         console.log(err);
         
@@ -105,79 +76,118 @@ exports.deleteUserAdmin = async (req, res, next) => {
         return res.status(400).json({status : false, message : 'Error : User not deleted'})
     }
 
-    let postDelete
-    try {
-        postDelete = await Post.deleteOne({authorId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Post not deleted'})
+    try {
+
+        await deletePost(req.body.params.userId)
+    } catch (err){
+
+        console.log(err)
     }
 
-    let postComDelete
-    try {
-        postComDelete = await PostCom.deleteOne({authorId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : UserCom not deleted'})
-    }
+    try {
 
-    let likeDelete
-    try {
-        likeDelete = await Like.deleteOne({userId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Like not deleted'})
-    }
+        await deleteTopic(req.body.params.userId)
+    } catch (err){
 
-    let topicDelete
-    try {
-        topicDelete = await Topic.deleteOne({ownerId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Topic not deleted'})
-    }
-
-    let topicMessageDelete
-    try {
-        topicMessageDelete = await TopicMessage.deleteOne({authorId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : TopicMessage not deleted'})
+        console.log(err)
     }
 
     let friendDelete
     try {
-        friendDelete = await Friend.deleteOne({receiverId : req.body.params.userId});
+        friendDelete = await Friend.deleteMany({$or: [{ receiverId: req.body.params_id }, { senderId: req.body.params_id }]});
     } catch (err) {
         console.log(err);
         
-        return res.status(400).json({status : false, message : 'Error : Friend not deleted'})
-    }
-
-    try {
-        friendDelete = await Friend.deleteOne({senderId : req.body.params.userId});
-    } catch (err) {
-        console.log(err);
-        
-        return res.status(400).json({status : false, message : 'Error : Friend not deleted'})
+        return res.status(400).json({status : false, message : 'Error : Friend not delete'})
     }
 
     return res.status(200).json({status : true, message : 'User deleted'})
 
 };
 
-async function deleteManyPostCom(PostId){
+async function deletePost(PostId){
+
+    // retrive users posts
+    let posts
+    try {
+        posts = await Post.find({authorId : PostId})
+    } catch (err){
+        console.log(err);
+    }
+
+    if(posts){
+        // delete postCom related to user posts
+        try {
+            await PostCom.deleteMany({postId : posts});
+        } catch (err) {
+            console.log(err);
+        }
+    
+        // delete like related to user posts
+        try {
+            await Like.deleteMany({postId : posts});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    // delete users posts
+    try {
+        await Post.deleteMany({authorId : PostId});
+    } catch (err) {
+        console.log(err);
+    }
+
+    // delete users postCom
     try {
         await PostCom.deleteMany({authorId : PostId});
     } catch (err) {
         console.log(err);
+    }
+
+    // delete users like
+    try {
+        await Like.deleteMany({userId : PostId});
+    } catch (err) {
+        console.log(err);
+    }
+
+    return {response : true, err : undefined};
+}
+
+async function deleteTopic(PostId){
+
+    // retrive users topics
+    let topics
+    try {
+        topics = await Topic.find({ownerId : PostId})
+    } catch (err){
+        console.log(err);
+    }
+
+    if(topics){
+        // delete topicMessage related to user topics
+        try {
+            await TopicMessage.deleteMany({topicId : topics});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    // delete users topics
+    try {
+        await Topic.deleteMany({ownerId : PostId});
+    } catch (err) {
+        console.log(err);
         
-        return {response : undefined, err : err};
+    }
+
+    // delete users topic message
+    try {
+        await TopicMessage.deleteMany({authorId : PostId});
+    } catch (err) {
+        console.log(err);
+        
     }
 
     return {response : true, err : undefined};
